de527c09506f83a376d11514f7e6f29fad8b3f4a
ligerfotis <ligerfotis@gmail.com>
2023-04-04 15:09:35 +0200

    change default value of ae type

--- utils.py
+++ utils.py
@@ -48,7 +48,8 @@ def get_args(parser):
     # argument for augmnentation
     parser.add_argument('-aug', '--augment', action='store_true', help='use data augmentation')
     # argument for the type of model
-    parser.add_argument('-at', '--ae-type', type=str, default='vae', help='type of autoencoder')
+    parser.add_argument('-at', '--ae-type', type=str, default='crvae', help='type of autoencoder')
+
     parser.add_argument('-bs', '--batch-size', default=512, type=int)
     parser.add_argument('-e', '--epochs', default=10, type=int)
     parser.add_argument('-lr', '--learning-rate', default=1e-5, type=float)
